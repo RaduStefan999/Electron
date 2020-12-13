2bef8e24621c0a99fed49870337bfeb97139a7fa
raducu999 <radustefan1302@gmail.com>
2020-12-13 15:11:12 +0200

    button begin

--- main.cpp
+++ main.cpp
@@ -7,6 +7,60 @@
 #define laturaPatrat 20
 using namespace std;
 
+struct Mouse
+{
+    int x, y;
+    bool LMBClick, RMBClick;
+};
+
+struct Collor
+{
+    unsigned short int r, g, b;
+};
+
+struct RectangleShape
+{
+    int x, y;
+    int width, height;
+    Collor collor;
+};
+
+struct Button
+{
+    RectangleShape shape;
+    Collor normalCollor;
+    Collor highlightCollor;
+
+};
+
+struct ButtonsList
+{
+    Button buttons[100];
+    int lg;
+};
+
+ButtonsList Buttons;
+
+bool contains (RectangleShape shape, Mouse mouse)
+{
+
+    if (mouse.x > shape.x && mouse.y > shape.y && mouse.x < shape.x + shape.width && mouse.y < shape.y + shape.height)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void draw (RectangleShape shape)
+{
+    setcolor(COLOR(shape.collor.r, shape.collor.g, shape.collor.b));
+    rectangle(shape.x, shape.y, shape.x + shape.width, shape.y + shape.height);
+
+}
+
+Mouse mouse;
+
 void setPattern(int x,int y)
 {
     int i,j;
@@ -95,7 +149,7 @@ void boardUpdate()
 {
 
     POINT cursorPosition,P;
-    if(ismouseclick(WM_LBUTTONDOWN)){
+    if(mouse.LMBClick){
         clearmouseclick(WM_LBUTTONDOWN);
         FILE *f;
 
@@ -111,15 +165,75 @@ void boardUpdate()
 
 }
 
+void buttonsUpdate()
+{
+    for (int i = 0; i < Buttons.lg; i++)
+    {
+        if (contains(Buttons.buttons[i].shape, mouse))
+        {
+            Buttons.buttons[i].shape.collor = Buttons.buttons[i].highlightCollor;
+            if (mouse.LMBClick)
+            {
+                //am apasat pe buton
+            }
+        }
+        else
+        {
+            Buttons.buttons[i].shape.collor = Buttons.buttons[i].normalCollor;
+        }
+
+        draw(Buttons.buttons[i].shape);
+    }
+}
+
+void mouseUpdate()
+{
+    mouse.x = mousex();
+    mouse.y = mousey();
+
+    mouse.LMBClick = mouse.RMBClick = false;
+
+    if(ismouseclick(WM_LBUTTONDOWN))
+    {
+        mouse.LMBClick = true;
+        clearmouseclick(WM_LBUTTONDOWN);
+    }
+
+    if(ismouseclick(WM_RBUTTONDOWN))
+    {
+        mouse.RMBClick = true;
+        clearmouseclick(WM_RBUTTONDOWN);
+    }
+}
+
 void openApp()
 {
-    DWORD xmax=GetSystemMetrics(SM_CXSCREEN);
-    DWORD ymax=GetSystemMetrics(SM_CYSCREEN);
+    int xmax=GetSystemMetrics(SM_CXSCREEN);
+    int ymax=GetSystemMetrics(SM_CYSCREEN);
+
     initwindow(xmax, ymax, "");
+
     setPattern(xmax,ymax);
+    Buttons.lg = 1;
+    Buttons.buttons[0].shape.x = 50;
+    Buttons.buttons[0].shape.y = 50;
+    Buttons.buttons[0].shape.width = 200;
+    Buttons.buttons[0].shape.height = 100;
+
+    Buttons.buttons[0].normalCollor.r = 100;
+    Buttons.buttons[0].normalCollor.g = 100;
+    Buttons.buttons[0].normalCollor.b = 100;
+
+    Buttons.buttons[0].highlightCollor.r = 200;
+    Buttons.buttons[0].highlightCollor.g = 200;
+    Buttons.buttons[0].highlightCollor.b = 200;
+    outtext("Dioda");
     while(!GetAsyncKeyState(VK_ESCAPE)){
-        boardUpdate();
+        mouseUpdate();
+        buttonsUpdate();
+        //boardUpdate();
     }
+
     closegraph();
 }
 int main()
