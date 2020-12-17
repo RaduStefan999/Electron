18195520199e5b8cbe0191a2b7b014405f68b31e
raducu999 <radustefan1302@gmail.com>
2020-12-17 11:27:18 +0200

    data strucure

--- headers/board.h
+++ headers/board.h
@@ -8,6 +8,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct connectionPoint
+{
+    int x, y, r;
+    connectionPoint *legatura;
+};
+
+struct boardElement
+{
+    char source[100];
+    int x, y, width, height;
+    int connectionPoints_nr;
+    connectionPoint connectionPoints[100];
+};
+
 void setPattern(int xa,int ya, int xb, int yb);
 void obtinePunctUtil(POINT A, POINT &B);
 void punePiesa(FILE *f, POINT c);
